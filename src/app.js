959e0b86502fba680a2cad8f16aa075f2e7e3131
Cristiano Andrade Franco <cristiano.franco.ext@ypioca.com.br>
2019-03-01 14:41:36 -0300

    CRUD

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -11,6 +15,26 @@ var route = router.get('/', (req, res, next) => {
     });
 });
 
+var create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+var put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id, 
+        item: req.body
+    });
+});
+
+var del = router.delete('/:id', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
+
 
 module.exports = app;
